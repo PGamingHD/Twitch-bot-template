37115e547e9f843bc60fd4e3ed0a4a34e76273b5
Pontus Karlsson <43955663+PGamingHD@users.noreply.github.com>
2023-12-14 16:09:48 +0100

    Delete build directory
    Removed build dir!

--- build/structures/Command.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Command = void 0;
-class Command {
-    constructor(options) {
-        Object.assign(this, Object.assign({}, options));
-    }
-}
-exports.Command = Command;
